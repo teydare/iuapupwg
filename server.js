3be675f2f83c1626f26b988d23fcbef73d73032a
teydare <fuddymain@gmail.com>
2026-01-26 21:01:54 +0000

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.set('trust proxy', 1);
 
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
